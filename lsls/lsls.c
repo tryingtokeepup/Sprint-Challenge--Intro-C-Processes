cc7b847be0822782470bdb8b6659a65edc356743
tryingtokeepup <kl.hawaii@gmail.com>
2019-04-26 10:14:39 -0700

    this is eating my soul. i got file names, file size, how the heck do i get them to get together?

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -1,28 +1,40 @@
 #include <stdio.h>
 #include <dirent.h>
-
+#include <sys/stat.h>
 /**
  * Main
  */
+// HINTS:
+// 1. stat() arguments require the path/filename UNLESS it is already at the path
+// 2. Concatenation of strings is C is not "normal" and a helper function is needed to do it accurately (so, to get the path/file-name string you need to do this).
+// 3. If the file is in directory `"."` and you concat the path, you are gunna have a bad time (see 1)
+// 4. Not ending a path with "/" may mean you end up having a bad time.
+
 int main(int argc, char **argv)
 {
   //Variables I am using:
   struct dirent *de = NULL;
+  struct stat buf;
   DIR *dr;
   // Parsing the Command Line
 
   printf("There are %d command line argument(s):\n", argc);
 
-  if (argc < 2)
-  {
-    printf("Testing: testing program <dirname>\n");
-    return 1;
-  }
-  dr = opendir(argv[1]);
   // Open directory
   // DIR *dr = opendir(".");
+  if (argv[1] == NULL)
+  {
+
+    dr = opendir(".");
+  }
+  else
+  {
+    dr = opendir(argv[1]);
+  }
+  // Print some file length
 
   // Repeatly read and print entries
+
   if (dr == NULL)
   {
 
@@ -31,8 +43,22 @@ int main(int argc, char **argv)
   }
   // Close directory
   while ((de = readdir(dr)) != NULL)
+  {
     printf("%s\n", de->d_name);
+    if (!stat(de->d_name, &buf))
+    {
 
+      printf("file size is %lld\n", buf.st_size);
+    }
+    else
+    {
+
+      printf("stat() failed for this file, try with another file.\n");
+    }
+
+    // stat("de", &buf);
+    // printf("file size is %lld\n", buf.st_size);
+  }
   closedir(dr);
   return 0;
 }

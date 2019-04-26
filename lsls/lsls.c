63f521107666b59063190662f111e76b34df607c
tryingtokeepup <kl.hawaii@gmail.com>
2019-04-26 11:18:16 -0700

    mvp done. i'm so spent.

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <dirent.h>
+#include <stdlib.h>
 #include <sys/stat.h>
+#include <string.h>
 /**
  * Main
  */
@@ -9,6 +11,14 @@
 // 2. Concatenation of strings is C is not "normal" and a helper function is needed to do it accurately (so, to get the path/file-name string you need to do this).
 // 3. If the file is in directory `"."` and you concat the path, you are gunna have a bad time (see 1)
 // 4. Not ending a path with "/" may mean you end up having a bad time.
+char *concat(const char *s1, const char *s2)
+{
+  char *result = malloc(strlen(s1) + strlen(s2) + 1); // +1 for the null-terminator
+  // in real code you would check for errors in malloc here
+  strcpy(result, s1);
+  strcat(result, s2);
+  return result;
+}
 
 int main(int argc, char **argv)
 {
@@ -16,6 +26,8 @@ int main(int argc, char **argv)
   struct dirent *de = NULL;
   struct stat buf;
   DIR *dr;
+  char *filename;
+
   // Parsing the Command Line
 
   printf("There are %d command line argument(s):\n", argc);
@@ -44,20 +56,32 @@ int main(int argc, char **argv)
   // Close directory
   while ((de = readdir(dr)) != NULL)
   {
-    printf("%s\n", de->d_name);
-    if (!stat(de->d_name, &buf))
+    // get around parsing issues
+    if (argv[1] != NULL)
     {
-
-      printf("file size is %lld\n", buf.st_size);
+      filename = concat(argv[1], de->d_name);
+      printf("filename: %s", filename);
     }
     else
     {
 
-      printf("stat() failed for this file, try with another file.\n");
+      filename = de->d_name;
     }
+    if (de->d_type != DT_DIR && !stat(filename, &buf))
+    {
 
-    // stat("de", &buf);
-    // printf("file size is %lld\n", buf.st_size);
+      printf("%s -- %10lli bytes\n", de->d_name, buf.st_size);
+      // printf("file size is %10lld\n", buf.st_size);
+    }
+    else if (de->d_type == DT_DIR)
+    {
+      printf("%s\n", de->d_name);
+    }
+    else
+    {
+      printf("%s\n", de->d_name);
+      printf("stat() failed.\n");
+    }
   }
   closedir(dr);
   return 0;
